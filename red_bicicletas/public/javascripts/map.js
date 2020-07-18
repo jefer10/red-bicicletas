d917955fcd6e03323da22945892285a46c9ec7b6
jefer <jefersson290@hotmail.com>
2020-07-17 23:57:32 -0500

    se añadio marker al mapa

--- red_bicicletas/public/javascripts/map.js
+++ red_bicicletas/public/javascripts/map.js
@@ -1,5 +1,9 @@
-var map = L.map('main_map').setView([51.505, -0.09], 13);
+var map = L.map('main_map').setView([-34.6012424, -58.3861497], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+var marker = L.marker([-34.6012424, -58.3861497]).addTo(map);
+var marker1 = L.marker([-34.6113424, -58.3861497]).addTo(map);
+var marker = L.marker([-34.6012424, -58.3764497]).addTo(map);
